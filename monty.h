4e741ebbf50121991a02cc0d6d3a27cf494b1a1c
muthamakicreations <muthamakicreations@gmail.com>
2023-06-27 10:41:44 -0700

    monty.h

--- monty.h
+++ monty.h
@@ -3,20 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
 #include <ctype.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 /**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
+ * struct stack_s - represents a stack
+ * @n: represents an integer
+ * @prev: shows the previous stack-element
+ * @next: shows the next stack-element
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: represents a doubly linked-list
  */
 typedef struct stack_s
 {
@@ -26,16 +25,28 @@ typedef struct stack_s
 } stack_t;
 
 /**
- * struct globals - global structure to use in the functions
- * @lifo: is stack or queue
- * @cont: current line
- * @arg: second parameter inside the current line
- * @head: doubly linked list
- * @fd: file descriptor
- * @buffer: input text
+ * struct instruction_s - contains the instructions.
+ * @opcode: represents opcode
+ * @f: represents the function
  *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
+ * Description: the instructions for the opcode
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * struct globals - represents the global structure
+ * @lifo: represents the stack
+ * @cont: shows the current line
+ * @arg: represents 2nd parameter in the current line
+ * @head: represents the doubly linked-list
+ * @fd: represents the file descriptor
+ * @buffer: the text to add
+ *
+ * Description: structure of the doubly linked-list
  */
 typedef struct globals
 {
@@ -47,57 +58,36 @@ typedef struct globals
 	char *buffer;
 } global_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
 extern global_t vglo;
 
-/* opcode_instructuions*/
-void _push(stack_t **stack, unsigned int line_number);
+/*all_prototypes*/
+void _add(stack_t **doubly, unsigned int cline);
+void _div(stack_t **doubly, unsigned int cline);
+void _mod(stack_t **doubly, unsigned int cline);
+void _mul(stack_t **doubly, unsigned int cline);
+void _nop(stack_t **doubly, unsigned int cline);
 void _pall(stack_t **stack, unsigned int line_number);
+void _pchar(stack_t **doubly, unsigned int cline);
 void _pint(stack_t **doubly, unsigned int cline);
 void _pop(stack_t **doubly, unsigned int cline);
-void _swap(stack_t **doubly, unsigned int cline);
-void _queue(stack_t **doubly, unsigned int cline);
-void _stack(stack_t **doubly, unsigned int cline);
-void _add(stack_t **doubly, unsigned int cline);
-void _nop(stack_t **doubly, unsigned int cline);
-void _sub(stack_t **doubly, unsigned int cline);
-void _div(stack_t **doubly, unsigned int cline);
-void _mul(stack_t **doubly, unsigned int cline);
-void _mod(stack_t **doubly, unsigned int cline);
-void _pchar(stack_t **doubly, unsigned int cline);
 void _pstr(stack_t **doubly, unsigned int cline);
+void _push(stack_t **stack, unsigned int line_number);
+void _queue(stack_t **doubly, unsigned int cline);
 void _rotl(stack_t **doubly, unsigned int cline);
 void _rotr(stack_t **doubly, unsigned int cline);
-
-/*get function*/
+void _stack(stack_t **doubly, unsigned int cline);
+void _sub(stack_t **doubly, unsigned int cline);
+void _swap(stack_t **doubly, unsigned int cline);
 void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
-
-/*imported functions*/
-int _sch(char *s, char c);
 char *_strtoky(char *s, char *d);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void *_calloc(unsigned int nmemb, unsigned int size);
 int _strcmp(char *s1, char *s2);
-
-/* doubly linked list functions */
 stack_t *add_dnodeint_end(stack_t **head, const int n);
 stack_t *add_dnodeint(stack_t **head, const int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void _swap(stack_t **doubly, unsigned int cline);
+int _sch(char *s, char c);
 void free_dlistint(stack_t *head);
-
-/* main */
 void free_vglo(void);
 
 #endif
